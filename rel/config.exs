f05b46fdc9092bd8dabb85257f0746312aad0bfe
kballou <kballou@devnulllabs.io>
2016-10-30 23:06:10 -0600

    Add release configuration
    Additionally, update the `.gitignore` file:
    
    *   Ignore artifacts under `rel/octochat`

---
+++ rel/config.exs
@@ -0,0 +1,19 @@
+use Mix.Releases.Config,
+    default_release: :default,
+    default_environment: :dev
+
+environment :dev do
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"[;|.U~9e:oy;Z[9HD?yQ0VP@tzR]+^|<5QToVo1=<V0/m[]*CIm*M_5D:PFj.gYy"
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+  set cookie: :"[;|.U~9e:oy;Z[9HD?yQ0VP@tzR]+^|<5QToVo1=<V0/m[]*CIm*M_5D:PFj.gYy"
+end
+
+release :octochat do
+  set version: current_version(:octochat)
+end
